c84d5420e2e1f6caa70f437c7b51800a958322f5
TpcBankk <55575947+TpcBankk@users.noreply.github.com>
2020-06-10 17:29:59 +0700

    calendar #2

--- tabsoft_test/resources/views/fullcalendar.blade.php
+++ tabsoft_test/resources/views/fullcalendar.blade.php
@@ -1,19 +1,18 @@
-@extends('layouts.app')
-@section('head')
+
+<html>
+<head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js" integrity="sha256-4iQZ6BVL4qNKlQ27TExEhBN1HFPvAvAMbFavKKosSWQ=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
-@endsection
-@section('content')
+</head>
+<body>
 <div class="container">
     <div class="response"></div>
     <div id='calendar'>hello</div>
 </div>
-
-@endsection
-@section('footer_scripts')
+</body>
 <script>
     $(document).ready(function() {
         var SITEURL = "{{url('/')}}";
@@ -98,6 +97,6 @@
         }, 1000);
     }
 </script>
-@endsection
+
 
 </html>
